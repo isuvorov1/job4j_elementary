db4a31d1d37245ad8f80f992ad0e5b191618f0ff
isuvorov1 <isuvorov1@outlook.com>
2021-07-17 16:57:29 -0400

    1. Модульные тесты. [#159435 #177704]

--- test/ru/job4j/MainTest.java
+++ test/ru/job4j/MainTest.java
@@ -1,14 +1,25 @@
 package ru.job4j;
 
+import org.junit.Assert;
 import org.junit.Test;
-import static org.hamcrest.CoreMatchers.is;
-import static org.junit.Assert.assertThat;
+import ru.job4j.converter.Converter;
 
 public class MainTest {
     @Test
-    public void whenActionThenResult() {
-        int excepted = 1;
-        int result = 1;
-        assertThat(result, is(excepted));
+    public void whenConvert140RblThen2Euro() {
+        float in = 140;
+        float expected = 2;
+        float out = Converter.rubleToEuro(in);
+        float eps = 0.0001f;
+        Assert.assertEquals(expected, out, eps);
+    }
+
+    @Test
+    public void whenConvert180RblThen3Dlr() {
+        float in = 180;
+        float expected = 3;
+        float out = Converter.rubleToDollar(in);
+        float eps = 0.0001f;
+        Assert.assertEquals(expected, out, eps);
     }
 }
