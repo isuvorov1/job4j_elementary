b673a67e82dd92e97e27ba3574bc0fab8ea396b8
isuvorov1 <isuvorov1@outlook.com>
2021-08-24 10:16:04 -0400

    6.0.5. Массивы в тестах. [#226789]

--- src/ru/job4j/array/Square.java
+++ src/ru/job4j/array/Square.java
@@ -3,14 +3,16 @@ package ru.job4j.array;
 public class Square {
     public static int[] calculate(int bound) {
         int[] rst = new int[bound];
+        for (int index = 0; index < rst.length; index++) {
+            rst[index] = index * index;
+        }
         return rst;
     }
 
     public static void main(String[] args) {
         int[] array = calculate(4);
         for (int index = 0; index < array.length; index++) {
-            array[index] = index;
-            System.out.println(array[index] * array[index]);
+            System.out.println(array[index]);
         }
     }
 }
